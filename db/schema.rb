2ee67ce4ddc81059313432a2ce68bc016bb12ce9
Leanne Lee <leannelee812@berkeley.edu>
2015-10-07 21:52:22 -0700

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302232135) do
+ActiveRecord::Schema.define(version: 20151008042157) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string   "dog_name"
+    t.string   "age"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
